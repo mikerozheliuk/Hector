04dbfaa1afa8427e89812bc05c9b926ba9fda275
mike <rozheluikmisha@gmail.com>
2024-07-02 11:32:08 +0300

    add autoplay to swiper

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -8,7 +8,7 @@
 // При необхідності підключаємо додаткові модулі слайдера, вказуючи їх у {} через кому
 // Приклад: { Navigation, Autoplay }
 import Swiper from "swiper";
-import { Navigation } from "swiper/modules";
+import { Navigation, Autoplay } from "swiper/modules";
 /*
 Основні модулі слайдера:
 Navigation, Pagination, Autoplay, 
@@ -35,7 +35,7 @@ function initSliders() {
       // Вказуємо склас потрібного слайдера
       // Підключаємо модулі слайдера
       // для конкретного випадку
-      modules: [Navigation],
+      modules: [Navigation, Autoplay],
       observer: true,
       observeParents: true,
       slidesPerView: 1,
@@ -50,11 +50,11 @@ function initSliders() {
       //lazy: true,
 
       // Ефекти
-      //   effect: "fade",
-      //   autoplay: {
-      //     delay: 3000,
-      //     disableOnInteraction: false,
-      //   },
+      effect: "fade",
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
 
       // Пагінація
       /*
